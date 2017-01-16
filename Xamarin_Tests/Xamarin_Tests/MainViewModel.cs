11c4733ab17b0cc30301b13d4fd413208cdb4413
adm.jthiriet <adm.jthiriet@W16005>
2017-01-16 17:13:12 +0100

    Photo

--- Xamarin_Tests/Xamarin_Tests/MainViewModel.cs
+++ Xamarin_Tests/Xamarin_Tests/MainViewModel.cs
@@ -4,24 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Prism.Mvvm;
 using Xamarin.Forms;
 
 namespace Xamarin_Tests
 {
-   public  class MainViewModel
+    public class MainViewModel : BindableBase
     {
+        private ImageSource _photo;
         public string Image { get; set; }
+
+        public ImageSource Photo
+        {
+            get
+            {
+                return _photo;
+            }
+            set
+            {
+
+                SetProperty(ref _photo, value);
+            }
+        }
+
         public ICommand LabelCommand { get; set; }
 
         public MainViewModel()
         {
             Image = FontAwesomeIcon.FaTrash.ToString();
-            LabelCommand = new Command(test);
+
+            LabelCommand = new Command(async () => await test());
+
         }
 
-        private void test()
+        private async Task test()
         {
-    
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported) return;
+
+            var options = new StoreCameraMediaOptions()
+            {
+                Directory = "Sample",
+                Name = "test.jpg",
+
+            };
+
+
+            var file = await CrossMedia.Current.TakePhotoAsync(options);
+            if (file == null)
+                return;
+
+
+            Photo = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
         }
     }
 }
